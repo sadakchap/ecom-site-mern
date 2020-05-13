3ce15b6d2c57e10fbbbb4edf4e512be8b3e2b1e1
sadakchap <aliceprerna@gmail.com>
2020-05-13 16:30:03 +0530

    fixed category update & delete route bug

--- routes/category.js
+++ routes/category.js
@@ -13,7 +13,7 @@ router.param("categoryId", getCategoryById);
 router.post("/category/create/:userId", isSignedIn, isAuthenticated, isAdmin, createCategory);
 router.get("/category/:categoryId", getCategory);
 router.get("/categories", getAllCategory);
-router.put("/category/:categoryTd/:userId", isSignedIn, isAuthenticated, isAdmin, updateCategory);
-router.delete("/category/:categoryTd/:userId", isSignedIn, isAuthenticated, isAdmin, removeCategory);
+router.put("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, updateCategory);
+router.delete("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, removeCategory);
 
 module.exports = router;
